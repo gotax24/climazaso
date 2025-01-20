34680e4692d73545bb425730406f84897f5e4100
gotax24 <124403304+gotax24@users.noreply.github.com>
2025-01-20 18:47:03 -0400

    haciendo el main de la app

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import Menu from "./components/Menu";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [city, setCity] = useState("");
+  const [data, setData] = useState(null);
+
+  const API_KEY = import.meta.env.VITE_API_KEY;
+
+  const weather = () => {
+    if (!city) return;
+
+    axios
+      .get(
+        `https://api.weatherstack.com/current?access_key=${API_KEY}&query=${city}`
+      )
+      .then((response) => {
+        console.log(response.data);
+        setData(response.data);
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Menu />
+      <input
+        type="search"
+        id="city"
+        value={city}
+        placeholder="Busca tu ciudad aqui"
+        onChange={(e) => setCity(e.target.value)}
+      />
+      <button onClick={weather}>Buscar</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Menu.jsx
@@ -0,0 +1,17 @@
+const Menu = () => {
+  return (
+    <>
+      <header>
+        <div className="header-logo">
+          <img className="logo" src="../public/logo.svg" alt="Logo de la pagina" />
+          <h1 className="title">Climazaso</h1>
+        </div>
+        <div className="header-subTitle">
+          <h2>Donde buscas el buen clima</h2>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Menu;
